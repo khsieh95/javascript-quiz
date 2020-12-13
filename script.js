82bfc72d6546c46ef337399ad44995407d47279a
Kevin Hsieh <khsieh95@gmail.com>
2020-12-13 12:56:46 -0800

    Created html and added pseudocode

---
+++ script.js
@@ -0,0 +1,7 @@
+// TODO:Create a start button
+// TODO:Click function start, starts timer
+// TODO:Click function, start quiz and display first question
+// TODO:4 questions total, one at a time
+// TODO:Answers will act as the button itself
+// TODO:If question is answered correctly, move on to next question, else, -10 on the timer, move on
+// TODO:If timer hits 0 before the end, your score is 0, else score = timer
