e032ee46479cc81b3dd9a1d5f1b07227a980e517
Kevin Hsieh <khsieh95@gmail.com>
2020-12-13 19:06:30 -0800

    fixed functions and if/else statements to read correct answer

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 // TODO:If question is answered correctly, move on to next question, else, -10 on the timer, move on
 // TODO:If timer hits 0 before the end, your score is 0, else score = timer
 
+// Set global variables
 var questContainer = document.querySelector(".question-container");
 var secondsLeft = 100;
 var homepage = document.querySelector("#homepage");
@@ -18,18 +19,15 @@ var questionsArray = [
   "Where is Rafael Nadal from?",
 ];
 var answersArray = [
-  "Roger Federer",
-  "Novak Djokovic",
-  "Pete Sampras",
-  "Rafael Nadal",
+  ["Roger Federer", "Novak Djokovic", "Pete Sampras", "Rafael Nadal"],
+  ["1", "2", "3", "4"],
+  ["Portugal", "Mexico", "Spain", "USA"],
 ];
-var answersArray2 = ["1", "2", "3", "4"];
-var answersArray3 = ["Portugal", "Mexico", "Spain", "USA"];
-
-// var correctAnswers = ["Roger Federer", "4", "Spain"];
 var questionCount = 0;
 var question = document.querySelector(".question");
-var correctAnswer = "Roger Federer";
+var correctAnswer = ["Roger Federer", "4", "Spain"];
+
+// Start button that starts the quiz when user is ready
 startButton.addEventListener("click", function () {
   setTime();
   homepage.remove();
@@ -37,52 +35,58 @@ startButton.addEventListener("click", function () {
   question.textContent = questionsArray[0];
   for (var i = 0; i < 4; i++) {
     var answer = document.querySelector(".btn" + (i + 1));
-    answer.textContent = answersArray[i];
+    answer.textContent = answersArray[questionCount][i];
   }
 });
 
-function nextQuestion() {
-  question.textContent = questionsArray[1];
+// function that runs to input second question in place of the first
+function secondQuestion() {
+  questionCount++;
+  question.textContent = questionsArray[questionCount];
   for (var i = 0; i < 4; i++) {
     var answer = document.querySelector(".btn" + (i + 1));
-    answer.textContent = answersArray2[i];
+
+    answer.textContent = answersArray[questionCount][i];
+
+    console.log("correct");
   }
 }
-
+// Buttons and functions that will follow the question
 var button1 = document.querySelector(".btn1");
 button1.addEventListener("click", function () {
-  if (button1.textContent === correctAnswer) {
+  if (button1.textContent === correctAnswer[questionCount]) {
   } else {
     secondsLeft -= 10;
   }
-  nextQuestion();
+  secondQuestion();
 });
 var button2 = document.querySelector(".btn2");
 button2.addEventListener("click", function () {
-  if (button2.textContent === correctAnswers) {
+  if (button2.textContent === correctAnswer[questionCount]) {
     console.log("hi");
   } else {
     secondsLeft -= 10;
   }
-  nextQuestion();
+  secondQuestion();
 });
 var button3 = document.querySelector(".btn3");
 button3.addEventListener("click", function () {
-  if (button3.textContent === correctAnswers) {
+  if (button3.textContent === correctAnswer[questionCount]) {
   } else {
     secondsLeft -= 10;
   }
-  nextQuestion();
+  secondQuestion();
 });
 var button4 = document.querySelector(".btn4");
 button4.addEventListener("click", function () {
-  if (button4.textContent === correctAnswers) {
+  if (button4.textContent === correctAnswer[questionCount]) {
   } else {
     secondsLeft -= 10;
   }
-  nextQuestion();
+  secondQuestion();
 });
 
+// Timer that counts down from 100 that will act as final score of user
 function setTime() {
   var timerInterval = setInterval(function () {
     secondsLeft--;
